3fe50e4ecee3eeff44d32d9107c0574a98403234
OwnNightmare <yura.borisov.9523@gmail.com>
2021-11-01 12:45:25 +0300

    made func create_folder

--- MyUploader.py
+++ MyUploader.py
@@ -5,6 +5,8 @@ from pprint import pprint
 class MyUploader:
     def __init__(self, token: str):
         self.token = token
+        self.common_url = 'https://cloud-api.yandex.net'
+        self.create_folder_url = '/v1/disk/resources'
 
     def get_headers(self):
         return {
@@ -12,8 +14,14 @@ class MyUploader:
             'Authorization': f'OAuth {self.token}'
         }
 
+    def create_folder_on_drive(self, path_on_drive):
+        headers = self.get_headers()
+        params = {'path': path_on_drive}
+        response = requests.put(url=f'{self.common_url}{self.create_folder_url}', params=params, headers=headers)
+        return response
+
     def get_upload_link(self, path_on_drive):
-        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        url = f'{self.common_url}/v1/disk/resources/upload'
         params = {'path': path_on_drive, 'overwrite': True}
         headers = self.get_headers()
         response = requests.get(url=url, params=params, headers=headers).json()
