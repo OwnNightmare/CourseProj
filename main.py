322e7a113adf51417d4453aa279ac83a24066268
OwnNightmare <yura.borisov.9523@gmail.com>
2021-11-01 12:46:49 +0300

    put code under funcs

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ with open('YandexToken.txt', encoding='utf8') as f:
     yan_token = f.read()
 
 yan_loader = MyUploader(yan_token)
-yan_loader.upload_file_to_ya_drive('Education/xxx', r'C:\Users\yurab\Git\Another dev.txt')
+
+
+def upload():
+    yan_loader.upload_file_to_ya_drive('Education/xxx', r'C:\Users\yurab\Git\Another dev.txt')
+
+
+def make_folder():
+    print(yan_loader.create_folder_on_drive('Education/Vk'))
