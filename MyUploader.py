d80e27bfc52ff25b3d2aa42ad4874597ad736deb
OwnNightmare <yura.borisov.9523@gmail.com>
2021-11-02 22:30:11 +0300

    changed uploading, now post, from url

--- MyUploader.py
+++ MyUploader.py
@@ -30,10 +30,17 @@ class MyUploader:
         r = requests.post(self.upload_url, params=params, headers=self.get_headers())
         print(r)
 
-    # def get_upload_link(self, path_on_drive):
-    #     url = self.upload_url
-    #     params = {'path': path_on_drive, 'overwrite': True}
-    #     headers = self.get_headers()
-    #     response = requests.get(url=url, params=params, headers=headers).json()
-    #     pprint(response)
-    #     return response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
